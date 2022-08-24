1a21e9c94da40ac3d23d8770d82fc830be74a066
Aryan Bhardwaj <aryan.bhardwaj1233@gmail.com>
2022-08-24 22:26:37 +0530

    updated Page1

--- src/components/Launch.tsx
+++ src/components/Launch.tsx
@@ -5,6 +5,8 @@ import {Link} from "react-router-dom"
 
 const Launch = () => {
 
+    // const image: HTMLImageElement = new Image();
+
     // declare intrface
     interface Launch {
         full_name: string;
@@ -13,6 +15,9 @@ const Launch = () => {
         launches: string;
         status: string;
         id: number;
+        images:any;
+        large: undefined;
+        // data type of image file
     }
     // declare state
     const [launches, setLaunches] = useState<Launch[]>([]);
@@ -39,6 +44,7 @@ const Launch = () => {
                 {launches.map(launch => (
                     <div className="launching" key = {launch.id}>
                         <div className="launch-details">
+                        <img src = {launch.images.large} alt = {launch.name} />
                         <h2>
                             {launch.full_name}
                         </h2>
@@ -49,18 +55,19 @@ const Launch = () => {
                         <br/>
                         <div className="status">
                         <h3>
-                            {launch.status}
+                            {/* If status is active it should be green */}
+                            {launch.status === "under construction" ? <><span className="under-construction">{launch.status}</span></> : launch.status === "active" ? <span className="active">{launch.status}</span> : <span className="inactive">{launch.status}</span>}
                         </h3>
                         <div className="active">
                         <p>
-                            <span>{launch.launches[0] ? <Link to = "/launchpad" >{launch.launches[0]}</Link> : <><p className="noLaunch">No Launch Available</p></>}</span>
+                            <span>{launch.launches[0] ? <>Launch 1 :<Link to = {`/launchpad/${launch.launches[0]}`} >{launch.launches[0]}</Link></> : <><p className="noLaunch">No Launch Available</p></>}</span>
                         </p>
                         <p>
-                            <span>{launch.launches[1] ? <Link to = "/launchpad" >{launch.launches[1]} </Link>: <><p >No Launch Available</p></>}</span>
+                            <span>{launch.launches[1] ? <> Launch 2 :<Link to = {`/launchpad/${launch.launches[1]}`} >{launch.launches[1]} </Link> </>: <><p >No Launch Available</p></>}</span>
                         </p>
                         <p>
                             <span>
-                            {launch.launches[2] ? <Link to = "/launchpad" >{(launch.launches[2])}</Link> : <><p className="noLaunch">No Launch Available</p></>}
+                            {launch.launches[2] ? <> Launch 3 :<Link to = {`/launchpad/${launch.launches[2]}`} >{(launch.launches[2])}</Link> </>: <><p className="noLaunch">No Launch Available</p></>}
                             </span>
                         </p>
                         </div>
